faeae09b1ab047516a46af1b9f23f98f7c985582
Дмитрий <kharitonovdo@yandex.ru>
2024-09-25 13:23:50 +0300

    main commit

--- main.py
+++ main.py
@@ -1,32 +1,12 @@
-import telebot
-import The_smart_part
+from flask import Flask, render_template
 
-bot = telebot.TeleBot('7224983182:AAHeT5I3WIhUIqnYOs6fUwhZXINi6wtpAIo');
-
-
-@bot.message_handler(commands=['start'])
-def welcome(message):
-    chat_id = message.chat.id
-    keyboard = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
-    button_support = telebot.types.KeyboardButton(text="Начать анализ")
-    button1 = telebot.types.KeyboardButton(text="поддержать автора")
-    keyboard.add(button_support, button1)
-
-    bot.send_message(chat_id,
-                     'Добро пожаловать в бота который подскажет Вам стоит ли покупать акцию',
-                     reply_markup=keyboard)
+app = Flask(__name__)
 
 
-@bot.message_handler(content_types=['text'])
-def get_text_messages(message):
-
-    if message.text == 'Начать анализ':
-        bot.send_message(message.from_user.id, text='@' + message.from_user.first_name)
-        question = 'давай начнем торговлю нужно ' + The_smart_part.main(The_smart_part.dt_now)
-        bot.send_message(message.from_user.id, text=question)
-    if message.text == 'поддержать автора':
-        question = '+79914010490'
-        bot.send_message(message.from_user.id, text=question)
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 
-bot.polling(none_stop=True, interval=0)
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
