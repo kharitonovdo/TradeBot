c5ed4ec950bcf998139a746fe2286b13085138ee
student <1@1.2>
2024-11-22 14:47:43 +0300

    buggggs

--- The_smart_part.py
+++ The_smart_part.py
@@ -6,13 +6,14 @@ dt_now = datetime.datetime.now()
 dt_now = str(dt_now).split(' ')[0]
 
 
-def prise(dataopen, dataclose):
+def prise(dataopen, dataclose, patern):
     a = requests.get(
         f'https://iss.moex.com/iss/history/engines/stock/markets/shares/'
-        f'boards/TQBR/securities/SBERP.json?from={dataopen}&till={dataclose}').json()
+        f'boards/TQBR/securities/{patern.upper()}.json?from={dataopen}&till={dataclose}').json()
     ansopen = []
     ansclose = []
     sps_stock = []
+    name = ''
     for i in a:
         b = a['history']
         deta = b['data']
@@ -20,10 +21,9 @@ def prise(dataopen, dataclose):
             sps_stock.append(j[3])
             ansopen.append(j[6])
             ansclose.append(j[11])
-    return ansopen[0], ansclose[-1], sps_stock[0]
+            name = j[2]
+    return ansopen[0], ansclose[-1], sps_stock[0], name
 
-
-print(prise('2024-06-01', '2024-09-01'))
 
 
 def comparison(dt, mon):
@@ -37,16 +37,18 @@ def comparison(dt, mon):
 print(comparison(dt_now, 1))
 
 
-def main(dn):
-    candle3 = prise(comparison(dn, 3), comparison(dn, 2))
-    candle2 = prise(comparison(dn, 2), comparison(dn, 1))
-    candle1 = prise(comparison(dn, 1), comparison(dn, 0))
+def main(dn, patern='SBERP'):
+    candle3 = prise(comparison(dn, 3), comparison(dn, 2), patern)[:-1]
+    candle2 = prise(comparison(dn, 2), comparison(dn, 1), patern)[:-1]
+    candle1 = prise(comparison(dn, 1), comparison(dn, 0), patern)
+    name = candle1[-1]
+    candle1 = candle1[:-1]
     print(candle3, candle2, candle1)
     if candle3[0] > candle2[0] and candle3[0] > candle1[0] or candle3[1] > candle1[1]:
-        return 'Нисходящий тренд'
+        return 'Нисходящий тренд', name
     if abs(candle3[0] - candle2[0]) <= 10 and abs(candle2[0] - candle1[0]) <= 10:
-        return 'Боковой тренд'
-    return 'Восходящий тренд'
+        return 'Боковой тренд', name
+    return 'Восходящий тренд', name
 
 
 print(main(dt_now))
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 import The_smart_part
 
 app = Flask(__name__)
@@ -9,9 +9,19 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/buy')
+@app.route('/buy', methods=['POST', 'GET'])
 def buy():
-    return render_template('buy.html', result=The_smart_part.main(The_smart_part.dt_now))
+    if request.method == 'POST':
+        #try:
+        return render_template('buy.html', result=The_smart_part.main(The_smart_part.dt_now, request.form.get("patern")[0]),
+                               name=The_smart_part.main(The_smart_part.dt_now, request.form.get("patern")))
+        # except Exception:
+        #     return redirect('abort')
+    return render_template('buy.html', result=The_smart_part.main(The_smart_part.dt_now)[0], name=The_smart_part.main(The_smart_part.dt_now)[1])
+
+@app.route('/abort')
+def abort():
+    return render_template('abort.html')
 
 
 if __name__ == '__main__':
